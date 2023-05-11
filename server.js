97628684a9acec04550d263e0830d49bbf2893d6
andreavirgilio99 <andrea.virgilio@tsi-srl.it>
2023-05-11 17:27:27 +0200

    ytdfyghj

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
-app.use(express.static('client_dist', {
-    setHeaders: (res, path) => {
-        if (path.endsWith('.js')) {
-            res.setHeader('Content-Type', 'application/javascript');
-        }
-    }
-}));
+const express = require('express');
+const app = express();
+const path = require('path');
 
-// Invia l'index.html del frontend per tutte le richieste GET che non corrispondono a una delle altre route definite
+// handling CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Serve i file statici della build di Angular
+app.use(express.static(path.join(__dirname, 'client_dist')));
+
+// Gestisci tutte le altre richieste inviando la pagina HTML del frontend
 app.get('*', (req, res) => {
-    res.sendFile('client_dist/index.html', { root: '.' });
+    res.sendFile(path.join(__dirname, 'client_dist', 'index.html'));
 });
 
 // route for handling requests from the Angular client
 app.get('/api/message', (req, res) => {
     res.json({ message: 'Hello GEEKS FOR GEEKS Folks from the Express server!' });
+});
+
+// Aggiungi l'header Content-Type nelle risposte per i file JavaScript
+app.get('*.js', function (req, res, next) {
+    res.type('text/javascript');
+    next();
 });
 
 app.listen(3000, () => {
