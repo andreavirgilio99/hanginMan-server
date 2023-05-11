7892673a72b491b73a29af5e8f4b6b9c714e3a84
andreavirgilio99 <andrea.virgilio@tsi-srl.it>
2023-05-11 17:16:54 +0200

    chatGPT's take on the issue

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
-const express = require('express');
-const app = express();
-  
-// handling CORS
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-  
 app.use(express.static('client_dist'));
 
-// Gestisci tutte le altre richieste inviando la pagina HTML del frontend
+// Invia l'index.html del frontend per tutte le richieste GET che non corrispondono a una delle altre route definite
 app.get('*', (req, res) => {
     res.sendFile('client_dist/index.html', { root: '.' });
-  });
+});
 
 // route for handling requests from the Angular client
 app.get('/api/message', (req, res) => {
     res.json({ message: 'Hello GEEKS FOR GEEKS Folks from the Express server!' });
 });
-  
+
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
