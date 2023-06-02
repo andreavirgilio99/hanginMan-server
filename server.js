abc763933c62f8339239593f393de5e9bcbebd5c
andreavirgilio99 <adrea.virgilio@tsigroupsrl.it>
2023-06-02 19:48:16 +0200

    ho fatto tante cose, ma non mi ricordo quali

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const server = http.createServer(app)
 
 const io = socketio(server, {
     cors: {
-      origin: ['http://localhost:4200', 'https://hanging-man.vercel.app']
+        origin: ['http://localhost:4200', 'https://hanging-man.vercel.app']
     }
-  })
+})
 
 const path = require('path');
 
@@ -23,19 +23,96 @@ app.use(express.static(path.join(__dirname, 'client_dist')));
 const Scopes = {
     GLOBAL: 'GLOBAL',
     ROOM: 'ROOM'
-  };
+};
 
-io.on('connection', socket =>{
+const users = new Map()
+
+const languages = new Map([ //language - roomObj[]
+    ['de', []],
+    ['en', []],
+    ['it', []],
+    ['es', []]
+])
+
+io.on('connection', socket => {
+    console.log("user connected")
     //socket.emit = send to only one, io.emit = send to everyone, socket.broadcaset.emit = send to everyone but that one connection
     socket.emit('welcome', 'welcome to the circus')
-    socket.broadcast.emit('user-join','some clown just joined lmao')
 
-    socket.on('disconnect', () =>{
-        io.emit('user-left', 'Good news fellas, some of you just fucked off hurray')
+    socket.on('disconnect', () => {
+        console.log("user disconnected")
+
+        const userData = users.get(socket.id);
+
+        if (userData) {
+            io.emit(userData.language + '-user-disconnected', socket.id)
+
+            if (userData.room != 'none') {
+                const room = languages.get(userData.language).find(rooms => rooms.id == userData.room)
+
+                if (room.peers.length == 1) {
+                    const indx = languages.get(userData.language).indexOf(room)
+
+                    if (indx != -1) {
+                        languages.get(userData.language).splice(indx, 1)
+                    }
+
+                    io.emit(userData.language + '-room-closed', room)
+                }
+                else {
+                    const indx = room.peers.findIndex(el => el == socket.id);
+
+                    if (indx != -1) {
+                        room.peers.splice(indx, 1)
+                    }
+
+                    io.emit(room.id + "-user-leave", socket.id) //cambia nome
+                }
+            }
+        }
     })
 
-    socket.on('message', message =>{
+    socket.on('message', message => {
+        console.log('message received')
+
         io.emit('message', message)
+    })
+
+    socket.on('join-country', language => {
+        console.log('a user joined a country')
+
+        users.set(socket.id, {
+            language: language,
+            room: 'none'
+        })
+
+        socket.broadcast.emit(language + '-user-connected', socket.id)
+        socket.emit('rooms-list', languages.get(language))
+    })
+
+    socket.on('create-room', data => { //data = [language, roomObj]
+        console.log('room created')
+
+        const language = data[0];
+        const room = data[1];
+
+        languages.get(language).push(room)
+        users.get(socket.id).room = room.id
+        io.emit(language + '-room-created', room)
+    })
+
+    socket.on('join-room', data => { //data = [language, roomId]
+        console.log('a user has joined a room')
+
+        const language = data[0];
+        const roomId = data[1];
+        const room = languages.get(language).find(el => el.id == roomId)
+
+        if(room){
+            room.peers.push(socket.id)
+        }
+
+        socket.broadcast.emit(language + '-user-join', roomId, socket.id) //cambia tutto
     })
 })
 
